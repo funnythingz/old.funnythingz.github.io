9158b73098065f1585129adfafa682e8658eba38
funnythingz <hiroki@hackleberry.me>
2014-07-09 13:51:29 +0900

    Fixed Gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -105,7 +105,7 @@ module.exports = function (grunt) {
     'copy'
   ]);
 
-  grunt.registerTask('deploy', ['build', 'master']);
+  grunt.registerTask('deploy', ['build', 'gh-pages']);
 
   grunt.registerTask('server', [
     'build',
